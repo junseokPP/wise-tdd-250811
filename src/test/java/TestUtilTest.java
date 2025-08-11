9152ca490a6136fc29844b974b9c1815ec0b5d63
junseokPP <pjs981130@gmail.com>
2025-08-11 12:13:31 +0900

    TestUtilTest t2 -red

--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +12,38 @@ public class TestUtilTest {
     @Test
     @DisplayName("TestUtil.genScanner()")
     void t1() {
+        Scanner sc = TestUtil.genScanner("""
+                        등록
+                        너 자신을 알라
+                        소크라테스
+                        """
+        );
+
+        String cmd = sc.nextLine();
+        String saying = sc.nextLine();
+        String author = sc.nextLine();
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(saying).isEqualTo("너 자신을 알라");
+        assertThat(author).isEqualTo("소크라테스");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    void t2() {
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        String outstr = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(outstr).isEqualTo("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        System.out.println("이제 화면에 출력됩니다.");
+
         Scanner sc = TestUtil.genScanner("""
                         등록
                         너 자신을 알라

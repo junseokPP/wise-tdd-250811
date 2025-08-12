e21e39f495b63127a67cf9114ae3535f392f7351
junseokPP <pjs981130@gmail.com>
2025-08-12 11:12:53 +0900

    step6 -red

--- src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -87,7 +87,7 @@ public class WiseSayingControllerTest {
         assertThat(out)
                 .contains("1번 명언이 삭제되었습니다.")
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
-                .doesNotContain("1 / 작자미상 / 현재를 사랑하라.");
+                .doesNotContain("1 / 작자미상 / 현재를 사랑하라."); //test5 삭제 작성
 
     }
 }
